3a736edf5419e1f3ac5a4b1e3e0ce12fdbe6ece5
Nick <efutin.kolya@yandex.ru>
2022-09-22 10:17:42 +0300

    домашка номер 2

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿var number1 = Decimal.Parse(Console.ReadLine());
+            var number2 = Decimal.Parse(Console.ReadLine());
+            if (number1 > number2)
+                Console.WriteLine(number1);
+            else 
+                Console.WriteLine(number2);
